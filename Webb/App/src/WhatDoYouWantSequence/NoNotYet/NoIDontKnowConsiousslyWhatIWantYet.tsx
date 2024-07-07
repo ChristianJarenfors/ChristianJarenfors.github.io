f09e8ba23af064b78e8386604db98309dfd814d0
Christian <christian.jarenfors@gmail.com>
2024-07-07 19:35:32 +0200

    fixifx

--- Webb/App/src/WhatDoYouWantSequence/NoNotYet/NoIDontKnowConsiousslyWhatIWantYet.tsx
+++ Webb/App/src/WhatDoYouWantSequence/NoNotYet/NoIDontKnowConsiousslyWhatIWantYet.tsx
@@ -4,14 +4,19 @@ function NoIDontKnowConsiousslyWhatIWantYet() {
   const handleOnClick = () => navigate("/AsSimplyAsJustKnowingWhatItIs");
   return (
     <header onClick={() => handleOnClick()}>
-      <h1 style={{ fontSize: "2em" }}>
+      <h1 style={{ fontSize: "2em", paddingRight: "3em" }}>
         Allow yourself to relax, Allow yourself to imagine and drop all thoughts
-        regaring resourses or the how's of getting what you want.
+        regaring resourses or the how's of getting what you want..
       </h1>
-      <h1>
-        Drift into a space of your mind where your though flow freely. A space
-        must people don't allow themselves to got to. Now close your eyes and
-        sit comfortably or lay down and do this. See what you can come up with..
+      <h1 style={{ fontSize: "3em" }}>
+        Drift into a space of your mind where your thoughts flow freely. <br />
+        <br />A space must people don't allow themselves to got to. <br />
+        <br />
+        Now close your eyes and sit comfortably or lay down and do this..
+        <br />
+        Imagine... <br />
+        <br />
+        See what you can come up with...
       </h1>
     </header>
   );
